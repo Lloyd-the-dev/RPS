7cda5a42fb375b233cc086c0455302002af72ba9
Favour Ojo <ojoore35@gmail.com>
2023-08-29 07:57:05 +0100

    fixed

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage(){
     return (
         <div className='flex flex-col items-center p-16 h-full bg-purp w-full'>
             <h1 className='text-3xl text-neutral-200'>ROCK / PAPER / SCISSORS</h1>
-            <img src="./public/images/gamer.png" alt="" className='mt-8'/>
+            <img src="/images/gamer.png" alt="" className='mt-8'/>
             <Link to="/test"><button className="capitalize bg-white p-4 w-64 mt-16 rounded-md font-bold text-black">start</button></Link>
             <br />
             <br />
